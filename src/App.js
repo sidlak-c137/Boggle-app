532ba6def74996fc5ed0cac6bf75938c6e9cbbc6
s-lakshmanansi <sidharth.m.lakshmanan@gmail.com>
2019-10-16 11:00:29 -0700

    Initial design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+export default function App() {
+    return (
+        <div className='appWindow'>
+            <div className='appLight'>
+                <head>
+                    <title>Sidharth's Boggle</title>
+                </head>
+                <body>
+                    <h1>Welcome to Boggle!</h1>
+                    <h3> by <br/> Sidharth Lakshmanan </h3>
+                </body>
+            </div>
+        </div>
+    );
 }
-
-export default App;
